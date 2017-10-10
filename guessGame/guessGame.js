996f6f806e8a69b0b682410a460b5f7c336b5da5
ccoombs161 <cameron.coombs97@gmail.com>
2017-10-10 12:59:46 -0400

    Fixed Guess button issue
    Fixed issue where the guess button wasn't working.

--- guessGame/guessGame.js
+++ guessGame/guessGame.js
@@ -23,27 +23,27 @@ return (guess > 0 && guess <= 10);
 
 function userGuessed() {
 var userGuessed = document.getElementById('userGuess').value;
-var statusArea = document.getElementById('statusArea');
+var gamestatus = document.getElementById('gamestatus');
 var historyList = document.getElementById('historyList');
 if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
     // Nothing entered or our of range.
-    writeMessage('statusArea', '<p>Please enter a number 1-10 and press the Guess button.</p>');
+    writeMessage('gamestatus', '<p>Please enter a number 1-10 and press the Guess button.</p>');
 } else if (userGuessed.indexOf('.') != -1) {
-    writeMessage('statusArea', '<p>Please enter a whole number 1-10 and press the Guess button.</p>');
+    writeMessage('gamestatus', '<p>Please enter a whole number 1-10 and press the Guess button.</p>');
 } else {
     numberOfGuesses++;
 
     if (userGuessed == secretNumber) {
         // Got it
-        writeMessage('statusArea', '<p>You got me in ' + numberOfGuesses +' guesses, I was thinking ' + secretNumber + '. Let\'s go again...</p>');
+        writeMessage('gamestatus', '<p>You got it in ' + numberOfGuesses +' guesses, the secret number was ' + secretNumber + '. Let\'s go again...</p>');
         newGame();
     } else if (userGuessed < secretNumber) {
         // User needs to guess higher
-        writeMessage('statusArea', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
+        writeMessage('gamestatus', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
         writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
     } else {
         // User needs to guess lower
-        writeMessage('statusArea', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
+        writeMessage('gamestatus', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
         writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
     }
 }
@@ -53,7 +53,7 @@ document.getElementById('userGuess').value = '';
 
 window.onload = function() {
 newGame();
-document.getElementById('buttonArea').addEventListener('click', userGuessed);
+document.getElementById('button').addEventListener('click', userGuessed);
 };
 /* window.onload event here because those are needed when the code will be 
 interacting with elements on the document, or DOM elements.*/
