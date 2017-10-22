1a784e19cceff821ab241d5072de35a8d4922a10
ccoombs161 <cameron.coombs97@gmail.com>
2017-10-22 14:59:26 -0400

    Fixed issues with taskAtHanda1a
    Fixed issue where task wouldn't display.
    Added in ability to delete, move task up/down.
    Cleaned up the code a bit.

--- taskAtHand1a/taskAtHand1a.js
+++ taskAtHand1a/taskAtHand1a.js
@@ -23,7 +23,7 @@ function taskAtHand1aApp()
 		})
 		.focus();
 
-		$("#app>header").append(version);
+		$("#app>header").append(version); 
 		setStatus("ready");
 	};
 } // end MyApp
@@ -41,17 +41,21 @@ function addTask()
 function addTaskElement (taskName)
 {
 	var $task = $("<li></li>");
-	$task.text(taskName);
 	$("#task-list").append($task);
+	var $delete = $("<button class='delete'>X</button>");
+	var $moveUp = $("<button class='move-up'>^</button>");
+	var $moveDown= $("<button class='move-down'>v</button>");
+	$task.append($delete)
+		 .append($moveUp)
+		 .append($moveDown)
+		.append("<span class='task-name'>" + taskName + "</span>");
+	$delete.click(function() { $task.remove(); });
+	$moveUp.click(function() { $task.insertBefore($task.prev()); });
+	$moveDown.click(function() { $task.insertAfter($task.next()); });
+	
 }
-/* 	JQuery's shorthand for the document ready event handler
-		could be written: $(document).ready(handler);
 
-		When this page loads, we'll create a global variable
-		named "app" by attaching it to the "window" object
-		(part of the BOM - Browser Object Model)
-*/
 $(function() {
-	window.app = new taskAtHand1aApp();
+	window.app = new taskAtHand1aApp ();
 	window.app.start();
 });
