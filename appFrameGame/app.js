faf81670add1ad667f74e262335153ed85e210fc
ccoombs161 <cameron.coombs97@gmail.com>
2017-10-23 15:09:06 -0400

    Added guessGame

--- appFrameGame/app.js
+++ appFrameGame/app.js
@@ -17,15 +17,68 @@ function MyApp()
 		$("#app>header").append(version);
 		setStatus("ready");
 	};
+
+var secretNumber;
+var numberOfGuesses = 0;
+
+function writeMessage(elementId, message, appendMessage) {
+	var elemToUpdate = document.getElementById('elementId');
+	secretNumber = document.getElementById('submitnum');
+	if (appendMessage) {
+		elemToUpdate.innerHTML = elemToUpdate.innerHTML + message;
+	} else {
+		elemToUpdate.innerHTML = message;
+	}
+};
+
+function newGame() {
+secretNumber = Math.floor(Math.random() * 10) + 1;
+numberOfGuesses = 0;
+writeMessage('historyList', '');
+}
+
+function guessInRange(guess) {
+return (guess > 0 && guess <= 10);
+}
+
+function userGuessed() {
+var userGuessed = document.getElementById('userGuess').value;
+var gamestatus = document.getElementById('gamestatus');
+var historyList = document.getElementById('historyList');
+
+if (userGuessed.length == 0 || ! guessInRange(userGuessed)) {
+    // Nothing entered or our of range.
+    writeMessage('gamestatus', '<p>Please enter a number 1-10 and press the Guess button.</p>');
+} else if (userGuessed.indexOf('.') != -1) {
+    writeMessage('gamestatus', '<p>Please enter a whole number 1-10 and press the Guess button.</p>');
+} else {
+    numberOfGuesses++;
+
+    if (userGuessed == secretNumber) {
+        // Got it
+        writeMessage('gamestatus', '<p>You got it in ' + numberOfGuesses +' guesses, the secret number was ' + secretNumber + '. Let\'s go again...</p>');
+        newGame();
+    } else if (userGuessed < secretNumber) {
+        // User needs to guess higher
+        writeMessage('gamestatus', '<p>You need to guess higher than ' + userGuessed + ', try again...</p>');
+        writeMessage('historyList', '<li>' + userGuessed +' (too low)</li>', true);
+    } else {
+        // User needs to guess lower
+        writeMessage('gamestatus', '<p>You need to guess lower than ' + userGuessed + ', try again...</p>');
+        writeMessage('historyList', '<li>' + userGuessed + ' (too high)</li>', true);
+    }
+}
+
+document.getElementById('userGuess').value = '';	
+}
+
+window.onload = function() {
+newGame();
+document.getElementById('button').addEventListener('click', userGuessed);
+};
+
 } // end MyApp
 
-/* 	JQuery's shorthand for the document ready event handler
-		could be written: $(document).ready(handler);
-
-		When this page loads, we'll create a global variable
-		named "app" by attaching it to the "window" object
-		(part of the BOM - Browser Object Model)
-*/
 $(function() {
 	window.app = new MyApp();
 	window.app.start();
